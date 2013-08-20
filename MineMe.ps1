66124f1582f7d2020b279bdda8136261324dda26
Avinash Sridhar <avinash.sridhar@gmail.com>
2013-08-20 17:38:46 -0400

    Initial import for MineMe
    A Powershell script that monitors the number of seconds a particular
    window is active and then logs data to a file. logs to csv with the
    window title and the total number of seconds presumed to be focussed.
    Polls every second. Does not log if it detects an idle time of more than
    60 seconds.

---
+++ MineMe.ps1
@@ -0,0 +1,80 @@
+﻿$IDLETIMESECONDS = 60               #number of seconds to consider me idle
+$OUTFILE = "c:\Temp\MineMe.csv"     # csv formatted data.
+
+Add-Type @"
+  using System;
+  using System.Runtime.InteropServices;
+  public class Tricks {
+    [DllImport("user32.dll")]
+    public static extern IntPtr GetForegroundWindow();
+}
+"@
+
+Add-Type @'
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace PInvoke.Win32 {
+
+    public static class UserInput {
+
+        [DllImport("user32.dll", SetLastError=false)]
+        private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct LASTINPUTINFO {
+            public uint cbSize;
+            public int dwTime;
+        }
+
+        public static DateTime LastInput {
+            get {
+                DateTime bootTime = DateTime.UtcNow.AddMilliseconds(-Environment.TickCount);
+                DateTime lastInput = bootTime.AddMilliseconds(LastInputTicks);
+                return lastInput;
+            }
+        }
+
+        public static TimeSpan IdleTime {
+            get {
+                return DateTime.UtcNow.Subtract(LastInput);
+            }
+        }
+
+        public static int LastInputTicks {
+            get {
+                LASTINPUTINFO lii = new LASTINPUTINFO();
+                lii.cbSize = (uint)Marshal.SizeOf(typeof(LASTINPUTINFO));
+                GetLastInputInfo(ref lii);
+                return lii.dwTime;
+            }
+        }
+    }
+}
+'@
+$hash = @{}
+while(1){
+    $idle =  [PInvoke.Win32.UserInput]::IdleTime
+    if($idle.TotalSeconds -lt $IDLETIMESECONDS){
+        $a = [tricks]::GetForegroundWindow()
+        #Get-Process | ? { $_.mainwindowhandle -eq $a } | Format-List MainWindowTitle | Out-File -Append $OUTFILE
+        $title = Get-Process | ? { $_.mainwindowhandle -eq $a } | Select-Object MainWindowTitle,id 
+        $key = $title.MainWindowTitle
+        if($key -ne $null){
+            if($hash.ContainsKey($key)){
+               $item = $hash.Get_Item($key);
+               $hash.Set_Item($key,$item+1);
+            }
+             else{
+                $hash.Add($key,1);
+            }
+            if($hash.Count -ge 1){
+               $hash.GetEnumerator() | Sort-Object Name |ForEach-Object {"{0},{1}" -f $_.Name,($_.Value -join ",")} |Out-File $OUTFILE
+                }
+
+        }
+        
+        Sleep 1
+    }
+}
